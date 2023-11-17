a6998fdfc2b0295810844862f7e5eeefed4dc42e
skinnyleg <haitamfox12@gmail.com>
2023-11-18 00:44:12 +0100

    changed the nickname from a random name to the login property

--- backend/trance-backend/src/utils/generateNickname.ts
+++ backend/trance-backend/src/utils/generateNickname.ts
@@ -1,6 +1,7 @@
 import { randomBytes } from "crypto";
 
 export const generateNickname = async (login: string): Promise<string> => {
-	const randomValue = randomBytes(4).toString('hex'); // Adjust the length of the random value as needed
-	return `${login}${randomValue}`;
+	// const randomValue = randomBytes(4).toString('hex'); // Adjust the length of the random value as needed
+	// return `${login}${randomValue}`;
+	return (login);
 }
