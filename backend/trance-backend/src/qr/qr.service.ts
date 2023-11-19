e91310d05be45b4aafb6d489f4b612a7718d70e1
skinnyleg <haitamfox12@gmail.com>
2023-11-19 20:53:25 +0100

    restored to prev versions

--- backend/trance-backend/src/qr/qr.service.ts
+++ backend/trance-backend/src/qr/qr.service.ts
@@ -26,8 +26,8 @@ export class QrService {
 		 if (isValid)
 		{
 			const token = await this.authService.createToken(user.id, user.login)
-			// res.cookie('token', token, {signed: true, sameSite: 'none'});
-			res.cookie('token', token);
+			res.cookie('token', token, {signed: true});
+			// res.cookie('token', token);
 			res.status(200).json(token);
 		   // return { valid: true, token: token };
 		 } else {
