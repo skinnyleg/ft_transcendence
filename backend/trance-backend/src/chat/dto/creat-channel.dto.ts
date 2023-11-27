8f3e9ec0a56c0213fc01f8ac9ff6d85f4cf1ce38
mohammed ed-dobaji <med-doba@mohammeds-MacBook-Pro.local>
2023-11-27 20:21:19 +0100

    creat dto to validate data in channel and message

---
+++ backend/trance-backend/src/chat/dto/creat-channel.dto.ts
@@ -0,0 +1,26 @@
+import { IsString, IsNotEmpty, IsOptional, IsEnum } from "class-validator";
+
+enum    channelType {
+    PUBLIC = 'public',
+    PROTECTED = 'protected',
+    PRIVATE = 'private'
+}
+
+export class creatChannelDto {
+
+    @IsNotEmpty()
+    @IsString()
+    name: string;
+  
+    @IsEnum(channelType)
+    type: string;
+  
+    @IsString()
+    @IsNotEmpty()
+    owner: string;
+  
+    @IsOptional()
+    @IsNotEmpty()
+    @IsString()
+    password?: string;
+}
