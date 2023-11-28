89aa9b6dd7184b797bf7ddc48dcb2c7faf310862
mohammed ed-dobaji <med-doba@mohammeds-MacBook-Pro.local>
2023-11-28 11:54:52 +0100

    init gateway and handle connection and dixsconnect of users

--- backend/trance-backend/src/chat/chat.module.ts
+++ backend/trance-backend/src/chat/chat.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ChatController } from './chat.controller';
-import { ChatService } from './chat.service';
 import { ChannelService } from './channel/channel.service';
 import { DmService } from './dm/dm.service';
 import { GatewayGateway } from './gateway/chat.gateway';
 
 @Module({
   controllers: [ChatController],
-  providers: [ChatService, ChannelService, DmService, GatewayGateway]
+  providers: [ChannelService, DmService, GatewayGateway]
 })
 export class ChatModule {}
--- backend/trance-backend/src/chat/gateway/chat.gateway.ts
+++ backend/trance-backend/src/chat/gateway/chat.gateway.ts
@@ -1,9 +1,31 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { ConnectedSocket, MessageBody, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { ChannelService } from '../channel/channel.service';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway()
-export class GatewayGateway {
+export class GatewayGateway implements OnGatewayConnection, OnGatewayDisconnect {
+
+  constructor(
+    private channelService: ChannelService
+  ){}
+
+  @WebSocketServer()
+  server: Server;
+
   @SubscribeMessage('message')
   handleMessage(client: any, payload: any): string {
     return 'Hello world!';
   }
+
+  async handleConnection(@ConnectedSocket() client: Socket)
+	{
+	   console.log('coonect');
+	   client.emit('userConnection', {msg: `a new user is connected ${client.id}`});
+  }
+    
+  async handleDisconnect(@ConnectedSocket() client: Socket)
+  {
+    console.log('disconnect');
+		client.disconnect();
+	}
 }
