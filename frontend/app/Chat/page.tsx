be8fca6e0c2cbf58b996f13cd1e6d06ef73635f7
Haitam Moubal <hmoubal@e2r2p13.1337.ma>
2023-12-16 20:04:22 +0100

    almost done with create channel

--- frontend/app/Chat/page.tsx
+++ frontend/app/Chat/page.tsx
@@ -11,22 +11,14 @@ export const metadata: Metadata = {
   viewport: 'width=device-width, initial-scale=1',
 }
 
-		{/* <div className='h-screen bg-green-600 flex flex-row pb-1 pr-2 lg:pb-1 xl:pb-1 lg:pt-0'> */}
-			{/* <TopBar /> */}
-			{/* <div className='w-full h-full xl:h-96 bg-yellow-600 flex flex-row pt-[70px] pb-1 pr-2 lg:pb-1 lg:pt-0'> */}
-			{/* 	<RightBar /> */}
-			{/* 	<Content /> */}
-			{/* 	<LeftBar /> */}
-			{/* </div> */}
-		// </div>
 interface ChatProps {}
 
 const chat: FC<ChatProps> = () => {
 	
 	return (
-		<div className='h-screen flex flex-col pt-[70px] pb-1 pr-2 lg:pb-1 lg:pt-2'>
+		<div className='h-screen flex flex-col'>
 			<TopBar />
-			<div className='h-[92%] w-full flex flex-row pb-1'>
+			<div className='h-[100%] lg:h-[88%] xl:h-[89%] min-[1024px]:h-[90%] w-full flex flex-row  pt-[70px] pb-1 pr-2 pl-4 lg:pb-1 lg:pt-2'>
 				<RightBar />
 				<Content />
 				<LeftBar />
--- frontend/app/ui/CreateChannelComponent.tsx
+++ frontend/app/ui/CreateChannelComponent.tsx
@@ -2,12 +2,15 @@
 import { Dialog, Transition } from '@headlessui/react'
 import { FC, Fragment, useState } from 'react'
 import { CreateChannelIcon } from './CustomIcons'
+import Image from 'next/image'
+import ChannelTypes from './ChannelTypeSelect'
 
 interface CreateChannelProps {}
 
 const CreateChannel: FC<CreateChannelProps> = () => {
 
   let [isOpen, setIsOpen] = useState(false)
+  let [type, setType] = useState('public')
 
   function closeModal() {
     setIsOpen(false)
@@ -16,6 +19,20 @@ const CreateChannel: FC<CreateChannelProps> = () => {
   function openModal() {
     setIsOpen(true)
   }
+  const handleTypeChange = (type: string) => {
+	console.log("type is ", type);
+	setType(type);
+  }
+
+  	const handleSubmit = (e) => {
+		e.preventDefault();
+	}
+//   const handleFileUpload = (e) => {
+// 		const selectedFile = e.target.files[0];
+
+// 		// Perform actions with the selected file, e.g., upload to a server
+// 		console.log('Selected File:', selectedFile);
+//   };
 
 	{/* <div className="fixed inset-0 flex items-center justify-center"> */}
       // </div>
@@ -32,7 +49,7 @@ const CreateChannel: FC<CreateChannelProps> = () => {
 		</button>
 
 		<Transition appear show={isOpen} as={Fragment}>
-			<Dialog as="div" className="relative z-10" onClose={closeModal}>
+			<Dialog as="form" className="relative z-10" onClose={closeModal}>
 				<Transition.Child
 					as={Fragment}
 					enter="ease-out duration-300"
@@ -63,41 +80,53 @@ const CreateChannel: FC<CreateChannelProps> = () => {
 								>
 									Create Channel
 								</Dialog.Title>
-								<div className="text-black mt-2 flex flex-col">
-									<h1>Name</h1>
+								<div className="text-black mt-2 flex gap-2 flex-col">
+									<div className='flex flex-row gap-3 items-end p-3'>
+										<Image
+											src='/GroupChat.png'
+											width={80}
+											height={80}
+											alt='Channel Picture'
+											className='rounded-full border-solid border-2 border-teal-500'
+										/>
+										<label className="border bg-cyan-400 rounded-2xl cursor-pointer w-32 h-fit mb-2 text-center">
+										<input
+											type="file"
+											accept="image/*"
+											className="hidden"
+											// onChange={(e) => handleFileUpload(e)}
+										/>
+										Upload Image
+										</label>
+									</div>
+									<h1 className='text-bold text-lg text-blue-900'>Name</h1>
 									<input placeholder='Channel Name'
-											type='text'
-											className=''
+										type='text'
+										className='rounded-2xl border-solid border-teal-200 focus:border-teal-500'
+										maxLength={10}
+										required
 									/>
-									<h1>Type</h1>
-									<input placeholder='Channel Type'
-											type='text'
-											className=''
+									<h1 className='text-bold text-lg text-blue-900'>Type</h1>
+									<ChannelTypes 
+										handleTypeChange={handleTypeChange}
 									/>
 
-									<h1>Password</h1>
-									<input placeholder='Channel Password'
+									<div className={`w-full h-fit ${type === 'protected' ? '': 'hidden'}`}>
+										<h1 className={`text-bold text-lg text-blue-900`}>Password</h1>
+										<input placeholder='Channel Password'
 											type='password'
-											className=''
-									/>
-									<input
-									type="file"
-									accept="image/*"
-									className=""
-									/>
-									<button 
-									className=""
-									>
-										Upload Image
-									</button>
+											className='rounded-2xl w-full border-solid border-teal-200 focus:border-teal-500'
+											required
+										/>
+									</div>
 								</div>
 
 								<div className='flex flex-row justify-end items-center gap-2'>
 									<div className="mt-4">
 										<button
-											type="button"
+											type="submit"
 											className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-green-300 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-											onClick={closeModal}
+											onClick={handleSubmit}
 										>
 										Submit
 										</button>
