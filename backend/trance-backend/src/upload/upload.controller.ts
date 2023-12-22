f82b19a0f76c394dab777a35b44dedc52be55d22
Haitam Moubal <hmoubal@e2r6p10.1337.ma>
2023-12-22 03:55:50 +0100

    tested upload for chat

--- backend/trance-backend/src/upload/upload.controller.ts
+++ backend/trance-backend/src/upload/upload.controller.ts
@@ -1,11 +1,12 @@
-import { Controller, Post, UseInterceptors, UploadedFile, Get, Param, Res, Req, BadRequestException, UseGuards } from '@nestjs/common';
+import { Controller, Post, UseInterceptors, UploadedFile, Get, Param, Res, Req, BadRequestException, UseGuards, Delete, ConflictException } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { backgroundPicMulterOptions, profilePicMulterOptions } from './multer.config';
+import { backgroundPicMulterOptions, channelPicMulterOptions, profilePicMulterOptions } from './multer.config';
 import * as path from 'path';
 import { Response } from 'express';
 import { UploadService } from './upload.service';
 import { getId } from 'src/utils/getId';
 import { JwtAuthGuard } from 'src/auth/jwt.guard';
+import * as fs from 'fs';
 
 @Controller('upload')
 export class UploadController {
@@ -20,7 +21,7 @@ export class UploadController {
 	if (file === undefined)
 		throw new BadRequestException('Server doesn\'t this upload')
 	const id = getId(req);
-	const newDir =  'http://localhost:8000/' + 'upload/Profile/'
+	const newDir =  'http://localhost:8000/' + 'upload/profile/'
 	const filePath = newDir + file.filename
 	await this.uploadService.updateProfilePic(filePath, id)
     return { valid:true, filename: filePath };
@@ -53,5 +54,39 @@ export class UploadController {
 	serveBackgroundPic(@Param('filename') filename: string, @Res() res: Response) {
 		const newDir =  path.join(__dirname, '..', '..', 'uploads', 'background')
     	res.sendFile(filename, { root: newDir });
+	}
+
+
+	@UseGuards(JwtAuthGuard)
+	@Post('channelPic')
+	@UseInterceptors(FileInterceptor('file', channelPicMulterOptions))
+	async uploadChannelPic(@UploadedFile() file: Express.Multer.File, @Req() req) {
+  
+	  if (file === undefined)
+		  throw new BadRequestException('Server doesn\'t this upload')
+	  const id = getId(req);
+	  const channelName = req.headers.channelname;
+	  if (channelName === undefined || channelName === '')
+		throw new BadRequestException('No Channel Name Provided')
+	  const newDir =  'http://localhost:8000/' + 'upload/profile/'
+	  const filePath = newDir + file.filename
+	  await this.uploadService.updateChannelPic(filePath, channelName)
+	  return { valid:true, filename: filePath };
+	}
+
+	@UseGuards(JwtAuthGuard)
+	@Delete('/channel/:filename')
+	deleteChannelPic(@Param('filename') filename: string, @Res() res: Response) {
+		const newDir =  path.join(__dirname, '..', '..', 'uploads', 'channels')
+		const fileToDelete = newDir + '/' + filename;
+		fs.unlink(fileToDelete, (err) => {
+			if (err) {
+				throw new ConflictException('Can\'t delete resource')
+			  console.error(err);
+			} else {
+			  console.log('File is deleted.');
+			}
+		  });
+    	res.status(204).send({msg: 'Deleted Successfully'});
 	}
 }
