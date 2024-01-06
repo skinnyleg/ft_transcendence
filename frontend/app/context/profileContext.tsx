ab25964a13805b08b4e269d7910d14f99f9c4406
zenon0777 <abderrahmane.daifi@protonmail.com>
2024-01-06 18:23:46 +0100

    profilePic context

--- frontend/app/context/profileContext.tsx
+++ frontend/app/context/profileContext.tsx
@@ -1,49 +1,46 @@
 'use client'
 
+import { usePathname } from "next/navigation";
 import React , { createContext, useEffect, useState } from "react";
-import { profileNickPic } from "../interfaces/interfaces";
 import { toast } from "react-toastify";
 
-export const picturesContext = createContext<any>({});
+export const FriendStatusContext = createContext<any>({});
 
-export const ContextProvider = ({ children }: { children: any })=> {
+
+export const ContextFriendProvider = ({ children }: { children: any })=> {
     
-    const [profilePic, setProfilePic] = useState('');
-    const [backgroundPic, setBackgroundPic] = useState('');
-    const [nickname, setNickname] = useState('');
+    const pathname = usePathname();
+    let nickname : string = pathname.split("/")[2];
+    console.log("niiimkm ===== ", nickname);
+    const [friendshipStatus, setFriendshipStatus] = useState('');
+    const getFriendshipStatus = async () => {
+      try {
+        const res = await fetch(`http://localhost:8000/user/FriendStatus`, {
+          method: "Post",
+          credentials: "include",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({nickname: nickname}),
+        });
+        if (res.ok) {       
+          const status = await res.json();
+          setFriendshipStatus(status.status);
+        }
+      } catch (error : any) {
+        console.log("data Ere", error.message)
+        toast.error(error.message);
+      }
+    };
     useEffect(() => {
-        const getnickname = async () => {
-          try {
-            const res = await fetch(`http://localhost:8000/user/Nickname`, {
-              method: "GET",
-              credentials: "include",
-              headers: { "Content-Type": "application/json" },
-            });
-            if (res.ok) {
-              const nickname : profileNickPic = await res.json();
-              setProfilePic(nickname.profilePic);
-              setBackgroundPic(nickname.BackgroundPic);
-              setNickname(nickname.nickname);
-              console.log("nick:", nickname);
-            }
-          } catch (error) {
-            toast.error('Error fetching data');
-          }
-        };
-        getnickname();
-      }, []);
-      const updateProfile = (newProfilePic: string, newBackground: string, newNickname: string) => {
-        if (newProfilePic)
-            setProfilePic(newProfilePic);
-        if (newBackground) setBackgroundPic(newBackground);
-        if (newNickname) setNickname(newNickname);
-      };
-      console.log("1***========", profilePic)
-      console.log("2***========", backgroundPic)
-      console.log("3***========", nickname)
+        getFriendshipStatus();
+      }, []);   
+      console.log("friendshipStatus: ", friendshipStatus);
+
+    const updateStatus = () => {
+      getFriendshipStatus();
+    }
     return (
-        <picturesContext.Provider value={{ profilePic, backgroundPic, nickname, updateProfile }}>
+        <FriendStatusContext.Provider value={{friendshipStatus, updateStatus}}>
         {children}
-      </picturesContext.Provider>
+      </FriendStatusContext.Provider>
     );
 }
