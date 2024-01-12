b991edecef44ce4559bf5856ca9cd6c8b43acf79
zenon0777 <abderrahmane.daifi@protonmail.com>
2024-01-12 04:16:45 +0100

    Accept/Refuse challenge and some changes

--- frontend/app/game/ui/GameResultBar.tsx
+++ frontend/app/game/ui/GameResultBar.tsx
@@ -6,36 +6,34 @@ import { playerInfo } from "../types/interfaces";
 
 
 const GameResultBar = () => {
-    return null;
+    const [playerL, setPlayerL] = useState<playerInfo | undefined>(undefined);
+    const [playerR, setPlayerR] = useState<playerInfo | undefined>(undefined);
 
-    // const [playerL, setPlayerL] = useState<playerInfo | undefined>(undefined);
-    // const [playerR, setPlayerR] = useState<playerInfo | undefined>(undefined);
-
-    // useEffect(() => {
-    //     // console.log('here')
-    //     gameSocket.emit('players-data');
-    //     gameSocket.on('players-info', (data: playerInfo[]) => {
-    //         // console.log('ops here == ', data);
-    //         setPlayerL(data[0]);
-    //         setPlayerR(data[1]);
-    //     });
+    useEffect(() => {
+        // console.log('here')
+        gameSocket.emit('players-data');
+        gameSocket.on('players-info', (data: playerInfo[]) => {
+            // console.log('ops here == ', data);
+            setPlayerL(data[0]);
+            setPlayerR(data[1]);
+        });
 
 
-    //     return () => {
-    //         gameSocket.off('players-info');
-    //     };
+        return () => {
+            gameSocket.off('players-info');
+        };
 
-    // },[]);
+    },[]);
 
-    // const obj1: playerInfoProps = {style: '', textPos: '', playerInfo: playerL};
-    // const obj2: playerInfoProps = {style:'flex-row-reverse', textPos:'text-end', playerInfo: playerR};
-    // return (
-    //     <div className="bg-cyan-600  w-[100%] h-[18.5%] rounded-[10px] flex items-center justify-between">
-    //         <PlayerInfo {...obj1}/>
-    //         <MatchScore />
-    //         <PlayerInfo {...obj2}/>
-    //     </div>
-    // );
+    const obj1: playerInfoProps = {style: '', textPos: '', playerInfo: playerL};
+    const obj2: playerInfoProps = {style:'flex-row-reverse', textPos:'text-end', playerInfo: playerR};
+    return (
+        <div className="bg-cyan-600  w-[100%] h-[18.5%] rounded-[10px] flex items-center justify-between">
+            <PlayerInfo {...obj1}/>
+            <MatchScore />
+            <PlayerInfo {...obj2}/>
+        </div>
+    );
 }
 
 export default GameResultBar;
