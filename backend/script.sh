446712f20d6b25614255307f3e535b8378421db6
Haitam Moubal <hmoubal@e2r6p6.1337.ma>
2024-01-25 04:18:24 +0100

    project finished only needs last touches and cleanup of the code

--- backend/script.sh
+++ backend/script.sh
@@ -1,13 +1,8 @@
 
 
-# cd /home/backend/src
-
-# npx prisma generate
-
-# npx prisma migrate dev
 npx prisma migrate deploy
 
-# npm run build
+npm run build
 
-# npm run start
-npm run start:dev
+npm run start
+# npm run start:dev
